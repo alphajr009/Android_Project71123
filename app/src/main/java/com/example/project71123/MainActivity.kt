c17013d28013b8f897201d80a264175639700741
Pasindu Randika <it21354410@my.sliit.lk>
2023-11-08 14:52:49 +0530

    APK | Hello Activity

--- app/src/main/java/com/example/project71123/MainActivity.kt
+++ app/src/main/java/com/example/project71123/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.project71123
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Button Click start HelloActivity
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+
+            val intent = Intent(this@MainActivity, HelloActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
