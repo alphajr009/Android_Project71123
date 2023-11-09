72c9e13780d98d793ea9964500812f68cbbee6b9
Pasindu Randika <it21354410@my.sliit.lk>
2023-11-10 00:13:51 +0530

    APK | Message Layout

--- app/src/main/java/com/example/project71123/HelloActivity.kt
+++ app/src/main/java/com/example/project71123/HelloActivity.kt
@@ -1,8 +1,10 @@
 package com.example.project71123
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
 
 class HelloActivity : AppCompatActivity() {
 
@@ -11,7 +13,15 @@ class HelloActivity : AppCompatActivity() {
         setContentView(R.layout.activity_hello)
 
         val tableName = intent.getStringExtra("tableName")
-        // Handle the clicked table here based on the tableName
-        Toast.makeText(this, "Clicked on $tableName", Toast.LENGTH_SHORT).show()
+
+        val materialToolbar: MaterialToolbar = findViewById(R.id.messageActualToolbar)
+        materialToolbar.title = tableName
+
+        materialToolbar.setNavigationOnClickListener {
+
+            val mainActivityIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainActivityIntent)
+        }
+
     }
 }
