4d1b9eb7e3793ba8d75ad2a235e4ca09679d5510
Pasindu Randika <it21354410@my.sliit.lk>
2023-11-11 16:42:38 +0530

    APK | Message Screen

--- app/src/main/java/com/example/project71123/HelloActivity.kt
+++ app/src/main/java/com/example/project71123/HelloActivity.kt
@@ -2,8 +2,8 @@ package com.example.project71123
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.Toast
 import com.google.android.material.appbar.MaterialToolbar
 
 class HelloActivity : AppCompatActivity() {
@@ -15,13 +15,24 @@ class HelloActivity : AppCompatActivity() {
         val tableName = intent.getStringExtra("tableName")
 
         val materialToolbar: MaterialToolbar = findViewById(R.id.messageActualToolbar)
-        materialToolbar.title = tableName
+
+
+        val textView8: TextView = findViewById(R.id.textView8)
+        val tableNumber = tableName?.let { extractTableNumber(it) }
+        textView8.text = tableNumber
+
 
         materialToolbar.setNavigationOnClickListener {
-
             val mainActivityIntent = Intent(this, MainActivity::class.java)
             startActivity(mainActivityIntent)
         }
+    }
 
+
+    private fun extractTableNumber(tableName: String): String {
+
+        val numberRegex = Regex("\\d+")
+        val matchResult = numberRegex.find(tableName)
+        return matchResult?.value ?: ""
     }
 }
